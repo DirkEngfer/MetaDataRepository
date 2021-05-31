888736f6cd6e6569bcf91437c81915d4fdb050f9
Dirk Engfer <de4769@yahoo.de>
2021-05-31 14:33:49 +0200

    Add files via upload

--- query_results_formatter.py
+++ query_results_formatter.py
@@ -27,3 +27,18 @@ with open('SDTM_CM_variables_sort_order.txt', 'rt') as f:
 out.close()
 os.remove("./SDTM_CM_variables_sort_order.txt")
 os.rename('q_out.txt', 'SDTM_CM_variables_sort_order.txt')
+
+out = open('q_out.txt', 'w')
+with open('SDTM_Convert_CMSTDTC_to_ATM.txt', 'rt') as f:
+  i = 0
+  for line in f:
+    if i >= 3:
+      if line[0:1] == '|':
+        text = line.replace('| ', '').replace('|', '').replace('d:', '').replace('"', '')
+        text = ' '.join(filter(None,text.split(' ')))
+        out.write(text)
+    i +=1
+out.close()
+os.remove("./SDTM_Convert_CMSTDTC_to_ATM.txt")
+os.rename('q_out.txt', 'SDTM_Convert_CMSTDTC_to_ATM.txt')
+
