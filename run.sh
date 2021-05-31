626edd7b347f177f89362646eadb2e88b01e46a9
Dirk Engfer <de4769@yahoo.de>
2021-05-31 14:44:20 +0200

    Debug run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 arq --data test.ttl --query test.rq > SDTM_CM_variables.txt ;
-arq --data test.ttl --query Order_of_SDTM_vars.rq > SDTM_CM_variables_sort_order.txt ; python query_results_formatter.py ;
+arq --data test.ttl --query Order_of_SDTM_vars.rq > SDTM_CM_variables_sort_order.txt ;
 arq --data test.ttl --data sdtm2adam_conversion.ttl --query CMSTDTC2ATM.rq > SDTM_Convert_CMSTDTC_to_ATM.txt ; python query_results_formatter.py ;
