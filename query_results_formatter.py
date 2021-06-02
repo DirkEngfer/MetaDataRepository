6d19d607afa16017f855f710bcb15c9c3d88394a
Dirk Engfer <de4769@yahoo.de>
2021-06-02 19:36:23 +0200

    Add files via upload
    Add query and metadata to select business rule corresponding to SAP or ADaM specs. Here, an imputation rule is added to handle lab values below quantifiable limit.

--- query_results_formatter.py
+++ query_results_formatter.py
@@ -42,3 +42,17 @@ out.close()
 os.remove("./SDTM_Convert_CMSTDTC_to_ATM.txt")
 os.rename('q_out.txt', 'SDTM_Convert_CMSTDTC_to_ATM.txt')
 
+out = open('q_out.txt', 'w')
+with open('imputations.txt', 'rt') as f:
+  i = 0
+  for line in f:
+    if i >= 3:
+      if line[0:1] == '|':
+        text = line.replace('| ', '').replace('|', '').replace('d:', '').replace('"', '')
+        text = ' '.join(filter(None,text.split(' ')))
+        out.write(text)
+    i +=1
+out.close()
+os.remove("./imputations.txt")
+os.rename('q_out.txt', 'imputations.txt')
+
